df4271818cfc61866c5ad8aeac084a9e58a81168
Ethan101Potter <polishchukserhii702@gmail.com>
2025-07-28 21:33:02 +0800

    feat: Add BranchLocator component to display branch locations and details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/hero-section";
 import ProductCategories from "@/components/product-categories"
+import BranchLocator from "@/components/branch-locator"
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <ProductCategories />
+      <BranchLocator />
     </>
   )
 }
